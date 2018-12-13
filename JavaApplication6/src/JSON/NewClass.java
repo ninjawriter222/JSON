3f10fe05b85571e6e744e4aa8c30c2764ce5d150
ninjawriter222 <devin.a.poe@gmail.com>
2018-12-12 17:54:45 -0700

    Initial commit

---
+++ JavaApplication6/src/JSON/NewClass.java
@@ -0,0 +1,22 @@
+
+package JSON;
+
+public class NewClass {
+    var family = [{
+    "Devin" : {
+    "name" : "Devin",
+    "age" : "28",
+    "gender" : "male"
+},
+    "Jeanette" : {
+    name : "Jeanette",
+    "age" : "26",
+    "gender" : "Female"
+},
+    "Evelyn" : {
+    "name" : "Evelyn",
+    "age" : "0",
+    "gender" : "Female"
+};
+}];
+}
